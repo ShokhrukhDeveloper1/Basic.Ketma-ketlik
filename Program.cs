d40296db472b365fe652b92c70de25c61a439565
Shokhrukh <122863833+ShokhrukhDeveloper1@users.noreply.github.com>
2023-12-27 01:11:19 +0500

    uchala masala ham .cs faylda

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Amaliy_vazifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1-masala. Doira yuzi va aylana uzunligini hisoblash
+
+            //int radius;
+            //Console.Write("radius=");
+            //radius = int.Parse(Console.ReadLine());
+            //double S, L;
+            //S = Math.PI * Math.Pow(radius, 2);
+            //L = 2 * Math.PI * radius;
+            //Console.Write($"S={S}, L={L}");
+
+            //2-masala. Valyuta konvertri
+
+            //double qiymat, kurs;
+            //Console.Write("qiymat=");
+            //qiymat = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("kurs=");
+            //kurs = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{qiymat*kurs} so'm");
+
+            //3-masala. Yoshni hisoblash
+
+            //int x;
+            //Console.Write("x=");
+            //x = int.Parse(Console.ReadLine());
+            //int hozirgi_yil = 2023;
+            //hozirgi_yil -= x;
+            //Console.WriteLine(hozirgi_yil * 365);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
